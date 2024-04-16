974f5df78b237c1ed5e62c56abcacaa70b5db7ce
eraygundogmus <eray.gundogmus@akinon.com>
2024-04-16 18:23:57 +0300

    feat: add basket

--- index.js
+++ index.js
@@ -1,116 +1,85 @@
-import {menuArray} from "/data.js"
+import { menuArray } from "/data.js";
 
-const menuSection = document.getElementById('menu-section')
+const menuSection = document.getElementById("menu-section");
 
 const selectedMarketItems = {
-    "button-id0": 1,
-    "button-id1": 0,
-    "button-id2": 0
-}
+  "button-id0": 1,
+  "button-id1": 0,
+  "button-id2": 0,
+};
 
 function createMenuCard(menuArray) {
-    return `
+  return `
         <section class="card">
             <div class="emoji">${menuArray.emoji}</div>
             <div class="card-left">
                 <h2>${menuArray.name}</h2>
-                <h3>${menuArray.ingredients.join(', ')}</h3>
+                <h3>${menuArray.ingredients.join(", ")}</h3>
                 <p>Price: $${menuArray.price}</p>
             </div>
-            <button class="add-button" id="${'button-id'+ menuArray.id}">+</button>
+            <button class="add-button" id="${
+              "button-id" + menuArray.id
+            }">+</button>
        </section>
     `;
 }
 
 function renderMenu() {
-    const menuCards = menuArray.map(menu => createMenuCard(menu));
-    menuSection.innerHTML = menuCards.join('');
+  const menuCards = menuArray.map((menu) => createMenuCard(menu));
+  menuSection.innerHTML = menuCards.join("");
 
-    const button1 = document.getElementById('button-id0')
-    const button2 = document.getElementById('button-id1')
-    const button3 = document.getElementById('button-id2')
-    const orderSection = document.getElementById('order-section')
+  const button1 = document.getElementById("button-id0");
+  const button2 = document.getElementById("button-id1");
+  const button3 = document.getElementById("button-id2");
+  const orderSection = document.getElementById("order-section");
 
+  const onButtonClick = (buttonId) => {
+    selectedMarketItems[buttonId] += 1;
 
+    const selectedItems = Object.entries(selectedMarketItems).filter(
+      (item) => item[1] > 0
+    );
 
+    const selectedItemsHTML = selectedItems.map((item) => {
+      const menuItem = menuArray.find(
+        (menu) => menu.id === +item[0].split("button-id")[1]
+      );
+      return `
+            <div class="small-card">
+                <div class="emoji">
+                    ${menuItem.emoji}     
+                    <div class=""titles> 
+                        <h2>${menuItem.name}</h2>
+                        <h3>${menuItem.ingredients.join(", ")}</h3>
+                    </div>        
+                    <div class="description">
+                      <p>Price: $${menuItem.price * item[1]}</p>
+                    </div>     
+                </div>
+            </div>
+        `;
+    });
 
+    const total = selectedItems.reduce((acc, item) => {
+      const menuItem = menuArray.find(
+        (menu) => menu.id === +item[0].split("button-id")[1]
+      );
+      return acc + menuItem.price * item[1];
+    }, 0);
 
-    
-    const onButtonClick = (id) => {
-        selectedMarketItems[id] =  selectedMarketItems[id] + 1
+    selectedItemsHTML.push(`<h2 class="total-price">Total: $${total}</h2>`);
+    orderSection.innerHTML = selectedItemsHTML.join("");
+  };
 
-
-
-        orderSection.innerHTML =  `<div> ${Object.keys(selectedMarketItems).filter((item) => selectedMarketItems[item] !== 0).map((key) => {
-          
-          
-            return `<div> 
-            ${menuArray.find((item) => {  
-                const regex = /id(\d+)$/;
-                const match = regex.exec(key);
-                const idItself = match ? match[1] : null;
-                return item.id == idItself
-                  }).name}  
-            ${menuArray.find((item) => {  
-                const regex = /id(\d+)$/;
-                const match = regex.exec(key);
-                const idItself = match ? match[1] : null;
-                return item.id == idItself
-                    }).emoji}   
-            ${menuArray.find((item) => {  
-            const regex = /id(\d+)$/;
-            const match = regex.exec(key);
-            const idItself = match ? match[1] : null;
-            return item.id == idItself
-                }).price} 
-                
-                ${menuArray.find((item) => {  
-                    const regex = /id(\d+)$/;
-                    const match = regex.exec(key);
-                    const idItself = match ? match[1] : null;
-                    return item.id == idItself
-                        }).price * selectedMarketItems[id]}$
-
-                
-                  </div>`
-        })} </div>`
-
-    
-
-
-        const selectedSingleItemCount = selectedMarketItems[id]
-
-        if (selectedSingleItemCount == 0) return null
-
-
-        const orderSectionHTML = `<div> 
-            ${Object.keys(selectedMarketItems).map}
-
-        </div>`
-    
-        
-
-        console.log('i am button', id)
-    }
-
-
-    button1.addEventListener("click", function(){
-        onButtonClick(button1.id)
-    }) 
-    button2.addEventListener("click", function(){
-        onButtonClick(button2.id)
-    }) 
-    button3.addEventListener("click", function(){
-        onButtonClick(button3.id)
-    }) 
-
-
+  button1.addEventListener("click", function () {
+    onButtonClick(button1.id);
+  });
+  button2.addEventListener("click", function () {
+    onButtonClick(button2.id);
+  });
+  button3.addEventListener("click", function () {
+    onButtonClick(button3.id);
+  });
 }
 
 renderMenu();
-
-
-
-
-
-
